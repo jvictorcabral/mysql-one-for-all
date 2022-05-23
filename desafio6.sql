63d6cb5930d7094806d6b22ce5d2f24b2f078450
Joao <joaovictorcgjvcg@gmail.com>
2022-05-23 14:04:16 -0400

    Corrigindo o requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,9 @@
-SELECT
+SELECT 
   MIN(p.price) AS faturamento_minimo,
   MAX(p.price) AS faturamento_maximo,
   ROUND(AVG(p.price), 2) AS faturamento_medio,
   SUM(p.price) AS faturamento_total
 FROM
-  SpotifyClone.plans AS p;
+  SpotifyClone.users AS us
+    JOIN
+  SpotifyClone.plans AS p ON p.plan_id = us.plan_id;
